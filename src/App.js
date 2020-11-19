24c43b595035d91da3086e77756399e4953aaf75
Ali Madooei <madooei@jhu.edu>
2020-11-19 09:04:12 -0500

    Add starter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+const meetings = [
+  {
+    _id: "5faf5ab7043e87536e31e54e",
+    course: "EN.601.226 Data Structure",
+    instructor: "Ali Madooei",
+    time: "MWF 12:00 - 1:15 PM",
+    link: "https://wse.zoom.us/j/91907049828",
+  },
+  {
+    _id: "5faf5ab7043e87536e31e54f",
+    course: "EN.601.226 Data Structure",
+    instructor: "Ali Madooei",
+    time: "MWF 1:30 - 2:45 PM",
+    link: "https://wse.zoom.us/j/99066784665",
+  },
+  {
+    _id: "5faf5ab7043e87536e31e550",
+    course: "EN.601.280 Full-Stack JavaScript",
+    instructor: "Ali Madooei",
+    time: "TuTh 12:00 - 1:15 PM",
+    link: "https://wse.zoom.us/j/93926139464",
+  },
+  {
+    _id: "5faf5ab7043e87536e31e551",
+    course: "EN.601.280 Full-Stack JavaScript",
+    instructor: "Ali Madooei",
+    time: "TuTh 1:30 - 2:45 PM",
+    link: "https://wse.zoom.us/j/91022779135",
+  },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div style={{ color: "white" }}>{JSON.stringify(meetings)}</div>;
 }
 
 export default App;
